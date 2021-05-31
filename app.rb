e5e4644608f04c6f629c5b41c7aaa871ef856f0d
minirachel <rachel.a.levinson@gmail.com>
2021-05-31 14:26:46 -0400

    commit tree cb42c06d82cd8f90ef1c20ab5e9cc0720c4eee54

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        new_phrase = PigLatinizer.new
+        @piglatin = new_phrase.piglatinize(params[:user_phrase])
+
+        erb :results
+    end
+
 end
